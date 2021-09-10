608a882b2b756b1d5e9306d8934545e572203b35
Minikanko <bcgrhio@naver.com>
2021-09-10 19:07:18 +0900

    connection local maria DB

--- server.js
+++ server.js
@@ -1,39 +1,36 @@
 const express = require('express');
+const fs = require('fs');
+
 //파일들의 형식에 따라 내부 내용을 읽어서 사용할 수 있게 해주는 모듈
 // ex JSON, Raw,URLencoded 형식의 내용들을 파싱해줌
-const bodyParser = require('body-parser'); 
-const app = express();
+const bodyParser = require('body-parser');
 const port = process.env.port || 5000;
-
+const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/customers', (req,res) =>{
-    res.send(
-        [
-            {
-              'id' : '1',
-              'image' : 'https://placeimg.com/64/64/10',
-              'gender' : '남성',
-              'name' : '홍길동',
-              'age' : 23
-            },
-            {
-              'id' : '2',
-              'image' : 'https://placeimg.com/64/64/5',
-              'gender' : '남성',
-              'name' : '홍길동',
-              'age' : 24
-            },
-            {
-              'id' : '3',
-              'image' : 'https://placeimg.com/64/64/7',
-              'gender' : '여성',
-              'name' : '홍길동',
-              'age' : 25
-            }
-          ]
-    );
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
+connection.connect();
+
+
+app.get('/customers', (req, res) => {
+    connection.query(
+        "select * from customer",
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    )
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
